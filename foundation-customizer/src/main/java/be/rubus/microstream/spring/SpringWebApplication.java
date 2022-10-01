3879ca9412817ffd033dd3f50b627144d806e6e4
sam253narula <samarthnarula253@gmail.com>
2022-10-01 15:36:39 +0530

    first commit

---
+++ foundation-customizer/src/main/java/be/rubus/microstream/spring/SpringWebApplication.java
@@ -0,0 +1,28 @@
+package be.rubus.microstream.spring;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@SpringBootApplication
+public class SpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebApplication.class, args);
+    }
+    
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("EMS API").version(appVersion)
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
+
+}
